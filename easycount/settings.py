24d5b18af1be7deec34e9b95ffb32061296c9a14
Nocchia04 <apannocchia04@gmail.com>
2024-02-23 15:28:06 +0100

    Eliminazione django_heroku come package non esistente

--- easycount/settings.py
+++ easycount/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 import django_on_heroku
 import dj_database_url
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
